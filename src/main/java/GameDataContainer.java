552fbfe220a3c6194c574334da530731a3b37d0a
Amar <amar.ustavdija@gmail.com>
2023-07-31 12:56:19 +0200

    started working on data container

--- src/main/java/GameDataContainer.java
+++ src/main/java/GameDataContainer.java
@@ -1,10 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameDataContainer extends JPanel {
 
+    private JPanel across, down;
 
     public GameDataContainer() {
         this.setPreferredSize(KeisukeConstants.GAME_DATA_CONTAINER_DIMENSION);
+        this.setLayout(new BorderLayout());
+
+        across = new JPanel();
+        down = new JPanel();
+
+        FlowLayout flowLayout = new FlowLayout();
+        flowLayout.setAlignment(FlowLayout.LEFT);
+        across.setLayout(flowLayout);
+
+        JLabel acrossLabel = new JLabel("Across:");
+        acrossLabel.setPreferredSize(new Dimension(50,50));
+        across.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
+
+        across.add(acrossLabel);
+
+
+        this.add(across, BorderLayout.NORTH);
+
+
+
 
 
     }
